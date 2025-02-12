9a6e2b791a591d60481efefd8fcdcc9adb22791d
NICK-TECH <elise>
2025-02-11 21:58:30 -0500

    Add a terraform configuration for codebuild

--- provider.tf
+++ provider.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 4.16"
     }
   }
+  backend "s3" {
+    bucket         = "terraformstatefileharunfeb112025"  # Replace with your S3 bucket name
+    key            = "terraform.tfstate"          # State file location within the bucket
+    region         = "var.region"                  # AWS region
+  }
 
   required_version = ">= 1.2.0"
 }
